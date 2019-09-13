533d9586e13be08a06650cf31d0899a43bc6b4b9
Jacob <jacobdeansd@gmail.com>
2019-09-12 18:09:49 -0700

    added hooks to sign in and sign up as well as app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 import { signOutStart } from '../../redux/user/user.actions';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import {
   HeaderContainer,
   LogoContainer,
   OptionsContainer,
-  OptionLink,
+  OptionLink
 } from './header.styles';
 
 const Header = ({ currentUser, hidden, signOutStart }) => (
